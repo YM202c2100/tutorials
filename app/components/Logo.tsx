e8d7aa6ed540f1ebe345e20f9569fbc4cf9ade52
YM202c2100 <y.m202c2100@gmail.com>
2024-05-27 11:30:20 +0900

    [style] ロゴを黒丸に設定

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -2,7 +2,11 @@ import Link from "next/link"
 
 const Logo = ()=>{
   return(
-    <Link href="/">CB</Link>
+    <div className="bg-black w-16 h-16 rounded-full flex justify-center items-center">
+      <Link href="/" 
+            className="text-white font-bold text-2xl"
+      >CB</Link>
+    </div>
   )
 }
 
