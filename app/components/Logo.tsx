59b0ec22ac3347dcc60c1f9518fe68164d994c60
YM202c2100 <y.m202c2100@gmail.com>
2024-05-27 15:55:41 +0900

    [style] ホバー時に背景色がグラデーション的に変化するように追加

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,13 +1,18 @@
 "use client"
 
 import Link from "next/link"
+import { motion } from "framer-motion"
 
+
+const MotionLink = motion(Link)
 const Logo = ()=>{
   return(
     <div>
-      <Link href="/" 
-            className="text-white font-bold text-2xl bg-black rounded-full flex w-16 h-16 justify-center items-center"
-      >CB</Link>
+      <MotionLink
+        href="/" 
+        className="text-white font-bold text-2xl bg-black rounded-full flex w-16 h-16 justify-center items-center"
+        whileHover={{"backgroundColor":["#121212", "rgba(131,58,180,1)","rgba(253,29,29,1)","rgba(252,176,69,1)","rgba(131,58,180,1)", "#121212"]}}
+      >CB</MotionLink>
     </div>
   )
 }
