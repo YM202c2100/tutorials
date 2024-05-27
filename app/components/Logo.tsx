2fdda9a1e46c37516d3ea0fa336742e3150e7ef6
YM202c2100 <y.m202c2100@gmail.com>
2024-05-27 11:42:28 +0900

    [style] ロゴのスタイルをLinkコンポーネント自体に付与するよう修正
    以前ではロゴの文字以外にLinkの当たり判定がなかったため修正

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -2,9 +2,9 @@ import Link from "next/link"
 
 const Logo = ()=>{
   return(
-    <div className="bg-black w-16 h-16 rounded-full flex justify-center items-center">
+    <div>
       <Link href="/" 
-            className="text-white font-bold text-2xl"
+            className="text-white font-bold text-2xl bg-black rounded-full flex w-16 h-16 justify-center items-center"
       >CB</Link>
     </div>
   )
