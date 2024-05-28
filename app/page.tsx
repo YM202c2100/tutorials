c4db08f45b403fd869bc6e03a47284f1a27f4c53
YM202c2100 <y.m202c2100@gmail.com>
2024-05-28 14:06:09 +0900

    [feat] プロファイル画像を追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
 import NavBar from "./components/NavBar";
+import profilePic from '@/public/images/profile/developer-pic-1.png'
 
 export default function Page() {
-  return (
+  return (<>
     <NavBar/>
-  );
+    <Image src={profilePic} alt="Picture of developer"/>
+  </>);
 }
