bf4e75190feb7e1d3b8fb285583f9653cf3d84da
YM202c2100 <y.m202c2100@gmail.com>
2024-05-28 15:13:47 +0900

    [style] 左半分に画像を表示

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import profilePic from '@/public/images/profile/developer-pic-1.png'
 export default function Page() {
   return (<>
     <NavBar/>
-    <Image src={profilePic} alt="Picture of developer"/>
+    <div className="w-1/2">
+      <Image src={profilePic} alt="Picture of developer"  className="pl-32"/>
+    </div>
   </>);
 }
