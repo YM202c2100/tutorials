40862bbdb874313aff690a488bb06d68ea20f202
YM202c2100 <y.m202c2100@gmail.com>
2024-05-30 16:17:04 +0900

    [feat] コンポーネントがウィンドウビューポートに表示されているかどうかを検知可能になるよう変更

--- app/components/CountUpTo.tsx
+++ app/components/CountUpTo.tsx
@@ -1,11 +1,23 @@
+"use client"
+
+import { useInView } from "framer-motion"
+import { useEffect, useRef } from "react"
 
 interface CUTProps{
   value: number
 }
 
 const CountUpTo:React.FC<CUTProps> = ({value})=>{
+  const ref = useRef<HTMLSpanElement>(null)
+  const isInView = useInView(ref)
+
+  useEffect(()=>{
+    if(isInView){
+      console.log("inView")
+    }
+  },[isInView])
   return(
-    <span>{value}</span>
+    <span ref={ref}>{value}</span>
   )
 }
 
