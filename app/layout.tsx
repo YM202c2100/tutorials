7a02913ce851b19917a47b04c03efe2e16473c86
YM202c2100 <y.m202c2100@gmail.com>
2024-06-01 14:37:03 +0900

    [refactor] NavBarコンポーネントを前ページで共有できるようにlayoutとして配置するように変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import NavBar from "./components/NavBar";
 
 const montserrat = Montserrat({ subsets: ["latin"], weight: ["500"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>{children}</body>
+      <body className={montserrat.className}>
+        <NavBar/>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
-import NavBar from "./components/NavBar";
 import profilePic from '@/public/images/profile/developer-pic-1.png'
 import AnimatedTitle from "./components/AnimatedTitle";
 
 export default function Page() {
   return (<>
-    <NavBar/>
     <div className="flex items-center">
       <div className="w-1/2">
         <Image src={profilePic} alt="Picture of developer"  className="pl-32"/>
