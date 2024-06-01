4499d6e4d02149fa0414951c9ce48ce8e91bd61a
YM202c2100 <y.m202c2100@gmail.com>
2024-06-02 00:02:21 +0900

    [feat] Articleコンポーネントにホバー時に表示する画像を引数として渡せるように変更

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,10 +1,12 @@
 import Article from "../components/Article";
+import sampleThumbnail from "@/public/images/articles/form validation in reactjs using custom react hook.png"
 
 export default function Page(){
   return(
     <Article 
       title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
       url="/"
+      thumbnail={sampleThumbnail}
     />
   )
 }
--- app/components/Article.tsx
+++ app/components/Article.tsx
@@ -1,18 +1,24 @@
 import Link from "next/link"
+import Image, { StaticImageData } from "next/image"
 
 interface Props {
   title:string,
   url:string,
+  thumbnail:StaticImageData,
   className?:string
 }
 
-const Article:React.FC<Props> = ({title, url, className})=>{
-  return(
+const Article:React.FC<Props> = ({title, url, thumbnail, className})=>{
+  return(<>
     <Link 
       href={url} 
       target="_blank"
-      className={`hover:underline ${className}`}>{title}</Link>
-  )
+      className={`hover:underline ${className}`}
+    >
+      {title}
+    </Link>
+    <Image src={thumbnail} alt="sampleThumbnailImage"/>
+  </>)
 }
 
 export default Article
