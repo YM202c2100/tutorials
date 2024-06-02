cc0af2b8fae7b6f621b83823b1197802e3d9096e
YM202c2100 <y.m202c2100@gmail.com>
2024-06-02 18:35:00 +0900

    [feat] マウスがタイトルに触れると画像が表示されるように変更

--- app/components/Article.tsx
+++ app/components/Article.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link"
 import Image, { StaticImageData } from "next/image"
+import { useRef } from "react"
 
 interface Props {
   title:string,
@@ -11,16 +12,29 @@ interface Props {
 }
 
 const Article:React.FC<Props> = ({title, url, thumbnail, className})=>{
+
+  const thumbNailRef = useRef<HTMLImageElement>(null)
+
   return(<>
     <Link 
       href={url} 
       target="_blank"
       className={`hover:underline ${className}`}
-      onMouseMove={(e)=>{console.log(e.pageX)}}
+      onMouseMove={(e)=>{
+        console.log(e.pageX)
+        if(thumbNailRef.current){
+          thumbNailRef.current.style.display = "inline-block"
+        }
+      }}
     >
       {title}
     </Link>
-    <Image src={thumbnail} alt="sampleThumbnailImage"/>
+    <Image 
+      ref={thumbNailRef}
+      src={thumbnail}
+      alt="sampleThumbnailImage"
+      className="hidden"
+    />
   </>)
 }
 
