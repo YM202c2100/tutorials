8faeaf625fa9587a3d14407b951145e3b3ee6d43
YM202c2100 <y.m202c2100@gmail.com>
2024-06-03 11:59:59 +0900

    [feat] リスト形式でArticleを追加

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,12 +1,20 @@
 import Article from "../components/Article";
 import sampleThumbnail from "@/public/images/articles/form validation in reactjs using custom react hook.png"
+import sampleThumbnail2 from "@/public/images/articles/smooth scrolling in reactjs.png"
 
 export default function Page(){
-  return(
-    <Article 
-      title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
-      url="/"
-      thumbnail={sampleThumbnail}
-    />
-  )
+  return(<>
+    <li className="list-none space-y-3">
+      <Article 
+        title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling"
+        url="/"
+        thumbnail={sampleThumbnail}
+      />
+      <Article 
+        title="Silky Smooth Scrolling In Reactjs: A Step-By-Step Guide For React Developers"
+        url="/"
+        thumbnail={sampleThumbnail2}
+      />
+    </li>
+  </>)
 }
