ceb8769ea95d8fa4bae3a0a36abb093e0a9d44f8
YM202c2100 <y.m202c2100@gmail.com>
2024-06-06 23:59:33 +0900

    [refactor] ポップアップメニューに関するコンポーネントにSetStateActionをpropsとして渡さないで良いように、Logoコンポーネント内に配置

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -7,63 +7,55 @@ import { useState } from "react"
 import { DribbbleIcon, GithubIcon, LinkedInIcon, PinterestIcon, TwitterIcon } from "./Icons"
 import { AddUnderLine } from "./Helpers"
 
-
-const MotionLink = motion(Link)
-
 interface LogoProps{
   className?:string
-}
-
-interface PopupProps{
-  toggleMenu: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 interface CustomLinkProps{
   href:string
   title:string
-  toggleMenu:React.Dispatch<React.SetStateAction<boolean>>
-}
-
-const CustomLink:React.FC<CustomLinkProps> = ({href, title, toggleMenu})=>{
-  return(
-    <AddUnderLine 
-      href={href} 
-      lineStyle="bg-white h-[2px] -translate-y-[2px]"
-    >
-      <Link 
-        href={href}
-        className="text-white" 
-        onClick={()=>toggleMenu(false)}
-      >
-        {title}
-      </Link>
-    </AddUnderLine>
-  )
-}
-
-const PopupMenu:React.FC<PopupProps> = ({toggleMenu})=>{
-  return(
-    <div className="w-[60vw] h-[50vh] backdrop-blur-md bg-black/70 fixed top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 rounded-lg">
-      <nav className="flex items-center justify-center">
-        <TwitterIcon className="w-6 mr-3"/>
-        <GithubIcon className="w-6 mx-3"/>
-        <LinkedInIcon className="w-6 mx-3"/>
-        <PinterestIcon className="w-6 mx-3"/>
-        <DribbbleIcon className="w-6 mx-3"/>
-        <motion.a href="/" target="_blank" whileHover={{scale:1.1, y:-2}} whileTap={{scale:0.9}} className="w-6 ml-3">T</motion.a>
-      </nav>
-      <nav className="items-center flex flex-col space-y-2">
-        <CustomLink href="/" title="Home" toggleMenu={toggleMenu}/>
-        <CustomLink href="/about" title="About" toggleMenu={toggleMenu}/>
-        <CustomLink href="/projects" title="Projects" toggleMenu={toggleMenu}/>
-        <CustomLink href="/articles" title="Articles" toggleMenu={toggleMenu}/>
-      </nav>
-    </div>
-  )
 }
 
 const Logo:React.FC<LogoProps> = ({className})=>{
   const [isMenuOpen, toggleMenu] = useState<boolean>(false)
+  
+  const CustomLink:React.FC<CustomLinkProps> = ({href, title})=>{
+    return(
+      <AddUnderLine 
+        href={href} 
+        lineStyle="bg-white h-[2px] -translate-y-[2px]"
+      >
+        <Link 
+          href={href}
+          className="text-white" 
+          onClick={()=>toggleMenu(false)}
+        >
+          {title}
+        </Link>
+      </AddUnderLine>
+    )
+  }
+
+  const PopupMenu:React.FC = ()=>{
+    return(
+      <div className="w-[60vw] h-[50vh] backdrop-blur-md bg-black/70 fixed top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 rounded-lg">
+        <nav className="flex items-center justify-center">
+          <TwitterIcon className="w-6 mr-3"/>
+          <GithubIcon className="w-6 mx-3"/>
+          <LinkedInIcon className="w-6 mx-3"/>
+          <PinterestIcon className="w-6 mx-3"/>
+          <DribbbleIcon className="w-6 mx-3"/>
+          <motion.a href="/" target="_blank" whileHover={{scale:1.1, y:-2}} whileTap={{scale:0.9}} className="w-6 ml-3">T</motion.a>
+        </nav>
+        <nav className="items-center flex flex-col space-y-2">
+          <CustomLink href="/" title="Home" />
+          <CustomLink href="/about" title="About" />
+          <CustomLink href="/projects" title="Projects" />
+          <CustomLink href="/articles" title="Articles" />
+        </nav>
+      </div>
+    )
+  }
 
 
   return(
@@ -81,7 +73,7 @@ const Logo:React.FC<LogoProps> = ({className})=>{
         CB
       </motion.button>
 
-      {isMenuOpen && <PopupMenu toggleMenu={toggleMenu}/>}
+      {isMenuOpen && <PopupMenu/>}
     </div>
   )
 }
