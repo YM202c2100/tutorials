5b20a054acd2673dcb67d74cd037f672237702c4
YM202c2100 <y.m202c2100@gmail.com>
2024-06-06 23:13:50 +0900

    [refactor] リンクにアニメーションする下線を追加できるヘルパーコンポーネントを定義

---
+++ app/components/Helpers.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { usePathname } from "next/navigation"
+
+interface Props{
+  href:string
+  lineStyle:string
+  children?:React.ReactNode
+}
+
+const AddUnderLine:React.FC<Props> = ({href,lineStyle,children})=>{
+  const currentPath = usePathname()
+  return(<>
+    <div className={"group"}>
+      {children}
+      <div className={`group-hover:w-full transition-[width] duration-150 ease-in ${(currentPath===href)?"w-full":"w-0"} ${lineStyle}`}>&nbsp;</div>
+    </div>
+  </>)
+}
+
+export{AddUnderLine}
