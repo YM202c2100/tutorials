6e330aee4a8c1f94fe9d7802c87e1aee54159363
YM202c2100 <y.m202c2100@gmail.com>
2024-07-23 22:23:32 +0900

    [refactor] コールバック関数名をより分かりやすいように変更

--- app/components/Slider.tsx
+++ app/components/Slider.tsx
@@ -15,22 +15,22 @@ const Content:React.FC<ContentProps> = ({color})=>{
 }
 
 const Slider:React.FC = ()=>{
-  const [isDragStart, startDrag] = useState<boolean>(false)
+  const [isDragging, setDragging] = useState<boolean>(false)
   const [initialPageX, setInitialX] = useState<number>(0)
   const [prevScroll, setPrevScroll] = useState<number>(0)
   const carouselRef = useRef<HTMLDivElement>(null)
 
-  const dragging = (e:React.MouseEvent)=>{
-    if(carouselRef.current && isDragStart){
+  const scrollByDragging = (e:React.MouseEvent)=>{
+    if(carouselRef.current && isDragging){
       const draggedDistance = initialPageX - e.pageX
       const scrollVal = prevScroll + draggedDistance
       carouselRef.current.scrollLeft = scrollVal
     }
   }
 
-  const dragStartHandler = (e:React.MouseEvent) =>{
+  const dragStart = (e:React.MouseEvent) =>{
     e.preventDefault()
-    startDrag(true)
+    setDragging(true)
     setInitialX(e.pageX)
     if(carouselRef.current){
       setPrevScroll(carouselRef.current.scrollLeft)
@@ -42,10 +42,10 @@ const Slider:React.FC = ()=>{
       <div 
         ref={carouselRef} 
         className="flex space-x-1 overflow-hidden"
-        onMouseMove={dragging}
-        onMouseDown={dragStartHandler}
-        onMouseUp={()=>{startDrag(false)}}
-        onMouseLeave={()=>{startDrag(false)}}
+        onMouseMove={scrollByDragging}
+        onMouseDown={dragStart}
+        onMouseUp={()=>{setDragging(false)}}
+        onMouseLeave={()=>{setDragging(false)}}
       >
         <Content color={"bg-blue-400"}/>
         <Content color={"bg-red-400"}/>
