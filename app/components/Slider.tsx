560866b3ad6bdb59e84df03233923d2e76aef94b
YM202c2100 <y.m202c2100@gmail.com>
2024-07-23 13:47:42 +0900

    [feat] スライダーの中身を画面からはみ出すように配置

--- app/components/Slider.tsx
+++ app/components/Slider.tsx
@@ -1,8 +1,29 @@
 "use client"
 
+interface ContentProps{
+  color:string
+}
+const Content:React.FC<ContentProps> = ({color})=>{
+  return(
+    <div className={`
+      w-1/3 aspect-square
+      flex-shrink-0
+      ${color}`}>
+    </div>
+  )
+}
+
 const Slider:React.FC = ()=>{
   return(
-    <div>Slider</div>
+    <div>
+      <div className="flex">
+        <Content color={"bg-blue-400"}/>
+        <Content color={"bg-red-400"}/>
+        <Content color={"bg-green-400"}/>
+        <Content color={"bg-yellow-400"}/>
+        <Content color={"bg-gray-400"}/>
+      </div>
+    </div>
   )
 }
 
